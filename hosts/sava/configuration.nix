df0b3664bf646a1d4783a98685b1b84deacc4f0c
themadbit <marktanui75@gmail.com>
2025-07-24 21:29:11 +0300

    kill firefox

--- hosts/sava/configuration.nix
+++ hosts/sava/configuration.nix
@@ -57,9 +57,6 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  # Install firefox.
-  programs.firefox.enable = true;
-
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
