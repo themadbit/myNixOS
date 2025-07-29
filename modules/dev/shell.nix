708e21b1502f95d2bd480acc749f68ec7b149531
themadbit <marktanui75@gmail.com>
2025-07-29 16:48:48 +0300

    add fish aliases
    Signed-off-by: themadbit <marktanui75@gmail.com>

--- modules/dev/shell.nix
+++ modules/dev/shell.nix
@@ -3,6 +3,11 @@
 {
   programs.fish = {
     enable = true;
+    shellAliases = {
+      lg = "lazygit";
+      mkcd = "mkdir -p \$1 && cd \$1";
+      src = "cd /src";
+    };
   };
   users.defaultUserShell = pkgs.fish;
 }
