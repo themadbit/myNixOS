cf6eff2fd7d6bf326f8ee4378bb905352fdd6276
themadbit <marktanui75@gmail.com>
2025-07-29 18:00:42 +0300

    add git aliases
    Signed-off-by: themadbit <marktanui75@gmail.com>

--- modules/dev/shell.nix
+++ modules/dev/shell.nix
@@ -4,9 +4,17 @@
   programs.fish = {
     enable = true;
     shellAliases = {
+      # general aliases
       lg = "lazygit";
       mkcd = "mkdir -p \$1 && cd \$1";
       src = "cd /src";
+
+      # git aliases
+      grb = "git rebase";
+      grba = "git rebase --abort";
+      grbc = "git rebase --continue";
+
+      gst = "git status";
     };
   };
   users.defaultUserShell = pkgs.fish;
