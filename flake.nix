b7e6e9a80587bbcd9054591882d3b99cfea86ef2
themadbit <marktanui75@gmail.com>
2025-09-23 11:12:18 +0300

    add formatter

--- flake.nix
+++ flake.nix
@@ -4,18 +4,52 @@
   inputs = {
     # NixOS official package source, using the nixos-25.05 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+    # Pre-commit hooks
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    # Please replace my-nixos with your hostname
-    nixosConfigurations.sava= nixpkgs.lib.nixosSystem {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      pre-commit-hooks,
+      ...
+    }@inputs:
+    let
       system = "x86_64-linux";
-      modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
-        ./hosts/sava/configuration.nix
-        ./modules
-      ];
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      # Formatter for `nix fmt`
+      formatter.${system} = pkgs.nixfmt-rfc-style;
+
+      # Pre-commit hooks
+      checks.${system} = {
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            nixfmt-rfc-style.enable = true;
+          };
+        };
+      };
+
+      # Development shell with pre-commit
+      devShells.${system}.default = pkgs.mkShell {
+        inherit (self.checks.${system}.pre-commit-check) shellHook;
+        buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
+      };
+
+      # Please replace my-nixos with your hostname
+      nixosConfigurations.sava = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          # Import the previous configuration.nix we used,
+          # so the old configuration file still takes effect
+          ./hosts/sava/configuration.nix
+          ./modules
+        ];
+      };
     };
-  };
 }
