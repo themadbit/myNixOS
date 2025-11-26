d850003a4d48f1f2ab202c46ae212a68a85cb01a
themadbit <marktanui75@gmail.com>
2025-11-26 09:19:42 +0300

    add env variables: TELEPORT_LOGIN

--- hosts/sava/configuration.nix
+++ hosts/sava/configuration.nix
@@ -76,6 +76,10 @@
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
+
+  environment.variables = {
+    TELEPORT_LOGIN = "marktanui";
+  };
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
