dd352bb103657441f3033e8c86aaaada1fd22159
themadbit <marktanui75@gmail.com>
2026-01-19 11:26:12 +0300

    chore: clean grub menu generations

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -8,4 +8,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.memtest86.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub = {
+    configurationLimit = 5;
+  };
 }
