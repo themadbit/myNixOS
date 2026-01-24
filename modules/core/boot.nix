aacb2ec5f6ab049babd9d411531926554e26f28f
themadbit <marktanui75@gmail.com>
2026-01-24 17:01:37 +0300

    feat(grub): add generation limit

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -1,13 +1,12 @@
 {
-  pkgs,
-  lib,
-  config,
   ...
 }:
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.memtest86.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  # Limit the number of generations to keep.
   boot.loader.grub = {
     configurationLimit = 5;
   };
