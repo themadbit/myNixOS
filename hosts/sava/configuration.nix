e586cd2eee701843574f1a80728b845b5bc60e9a
themadbit <marktanui75@gmail.com>
2026-01-24 17:01:03 +0300

    feat(ssh): enable and use post-quantum algo

--- hosts/sava/configuration.nix
+++ hosts/sava/configuration.nix
@@ -96,8 +96,14 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
+  services.openssh = {
+    enable = true;
+    settings = {
+      KexAlgorithms = [
+        "mlkem768x25519-sha256"
+      ];
+    };
+  };
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
